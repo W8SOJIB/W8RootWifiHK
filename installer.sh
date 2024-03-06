5ab39e0b1114705a06391e23a2fbc2752b923a6c
W8SOJIB <92366482+W8SOJIB@users.noreply.github.com>
2024-03-06 13:18:36 +0600

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install -y root-repo 
+pkg install -y git tsu python wpa-supplicant pixiewps iw
+
+git clone --depth 1 https://github.com/W8SOJIB/W8RootWifiHK OneShot
+
+chmod +x W8RootWifiHK/oneshot.py
+
+printf "###############################################\n#  All done! Now you can run OneShot with\n#   sudo python OneShot/oneshot.py -i wlan0 -K\n#\n#  Join W8Team,\n#\n###############################################\n"
